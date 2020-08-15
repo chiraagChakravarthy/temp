bc2b1f06bdaf66f36eef2dfa9b8c0f146ae149e3
Chiraag Chakravarthy <beaverduck99@gmail.com>
2020-08-15 09:40:48 -0400

    added some stuff

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser);
+const sha256 = require('js-sha256');
+const now = require('nano-time');
+const fs = require('fs')
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.post('/register', (req, res) => {
-    console.log(req.body.name);
-    console.log('test');
-    res.send('Hello There');
+    var body = req.body;
+    var salt = now();//nanotime
+    var account = {
+        "linkedin": body.linkedin,
+        "birthday": body.birthday,
+        "speciality": body.speciality,
+        "password": {
+            "salt": salt,
+            "hash": sha256(body.password + salt)
+        }
+    }
+    var users = JSON.parse(readUsers());
+    users[body.name] = account;
+    writeUsers(JSON.stringify(users))
+    res.send('Ok');
 })
 
-app.listen(8080, () => console.log('listening on port 8080'));
+app.post('/register1', (req, res)=>{
+    fs.writeFileSync('test.txt')
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log('listening on port ' + PORT));
+
+
+
+function readUsers(){
+    var users = fs.readFileSync('users.json');
+    return users;
+}
+
+function writeUsers(users){
+    fs.writeFileSync('users.json', users)
+}
