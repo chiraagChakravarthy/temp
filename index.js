d4b2441e9ea8e51121463f7274782482b9a0a1de
Chiraag Chakravarthy <beaverduck99@gmail.com>
2020-08-15 23:35:18 -0400

    added stuff

--- index.js
+++ index.js
@@ -23,27 +23,60 @@ app.post('/register', (req, res) => {
     var usersString = readUsers()
 
     var users = JSON.parse(usersString);
-    console.log(users)
-    console.log(account)
-    users[body.name] = account;
-    writeUsers(JSON.stringify(users))
-    res.send('Ok');
+    if(users.hasOwnProperty(body.name)){
+        res.send('1')
+    } else {
+        users[body.name] = account;
+        writeUsers(JSON.stringify(users))
+        res.send('0')
+    }
+    
 })
 
 app.get('/users', (req, res) => {
-    console.log('got')
     res.write(fs.readFileSync('users.json'))
     res.end();
 })
 
-app.post('/register1', (req, res)=>{
+app.post('/login', (req, res) => {
+    var body = req.body;
+    var users = JSON.parse(readUsers());
 
+    if(users.hasOwnProperty(body.name)){
+        var password = users[body.name]['password'];
+        if(sha256(body.password + password.salt)==password.hash){
+            res.send('0')
+            console.log('0')
+        } else {
+            res.send('2')
+            console.log('2')
+        }
+    } else {
+        res.send('1')
+        console.log('1')
+    }
+    res.end()
 })
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('listening on port ' + PORT));
 
-
+app.get('/user', (req, res) => {
+    var body = req.body;
+    var users = JSON.parse(readUsers())
+    if(users.hasOwnProperty(body.name)){
+        var user = user[body.name]
+        res.write(JSON.stringify({
+            'name': user.name,
+            'birthday': user.birthday,
+            'speciality': user.speciality,
+            'linkedin': user.linkedin
+        }))
+    } else {
+        res.write(1)
+    }
+    res.end()
+})
 
 function readUsers(){
     var users = fs.readFileSync('users.json');
